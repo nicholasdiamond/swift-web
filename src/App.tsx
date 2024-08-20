e49b0bc6990a7832d0d067a9d08834779cb138ca
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-08-20 21:24:35 +0100

    Configured Tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./route";
+import "./index.css";
+import "./App.css";
 
 export default function App() {
-  const [user] = useState<boolean>(true);
+  const [user] = useState<boolean>(false);
   return (
     <>
       <RouterProvider router={routes(user)} />
