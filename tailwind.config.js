68e783974fe2b639f003b057d54dfbaf38ba3b44
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-08-21 13:15:23 +0100

    Configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "nav-background": " #E0F4FD",
+        "arrow-down": "#2C2D48",
+        active: "#3BB9F1",
+      },
+      boxShadow: {
+        "nav-shadow": "0px 0px 2px 0px #0000001A",
+      },
+    },
   },
   plugins: [],
 };
